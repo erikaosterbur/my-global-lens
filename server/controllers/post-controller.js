d401a3a6e3aa1ceca02027afc27f634d1c65c263
Laura Hofer <ljhofer@gmail.com>
2021-12-21 11:33:19 -0600

    Added create, update, and delete routes in the postController

--- server/controllers/post-controller.js
+++ server/controllers/post-controller.js
@@ -1,5 +1,58 @@
-const { Post } = require('../models');
+const { Post, User, Comment } = require('../models');
+
+// getAllPosts,
+//     getSinglePost,
+//     deletePost,
 
 module.exports = {
+
+    async createPost (req, res) {
+        const postObj = { img: req.body.img, country: req.body.country, caption: req.body.caption, postedBy: req.body.userId }
+        const newPost = await Post.create(postObj);
+
+        if (!newPost) {
+            return res.status(400).json({ message: "Unable to create post." });
+        }
+
+        res.status(200).json(newPost);
+    },
+
+    async editPost (req, res) {
+        const post = await Post.findOneAndUpdate (
+            { _id: req.params.id },
+            req.body, 
+            { new: true }
+        );
+
+        if (!post) {
+            return res.status(400).json({ message: "Unable to update post." });
+        }
+
+        res.status(200).json(post);
+    },
+
+    async deletePost (req, res) {
+        try {
+            const post = await Post.findOneById(req.params.id);
+            if (post.userId === req.body.userId) {
+                await post.deleteOne();
+                res.status(200).json( "The post has been deleted." );
+            } else {
+                res.status(403).json("You can only delete your own post." );
+            }    
+        } catch (err) {
+            res.status(500).json(err);
+        }
+
+
+
+       
+        
+        
+        
+    }
+
+
+
     
 }
